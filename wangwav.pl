e8a8b9d5cc4472046ea47244846ef9ae7043c474
David Okubo <okubod@oregonstate.edu>
2018-03-12 18:08:53 -0700

    cmd

--- wangwav.pl
+++ wangwav.pl
@@ -89,6 +89,9 @@ parent(selma,ling).
 % 1. Define the predicate `cmd/3`, which describes the effect of executing a
 %    command on the stack.
 
+cmd(C, S1, S2) :- S2 = [C | S1].
+cmd(add, [Stack1, Stack2 | S1], S2) :- Sum is (Stack1 + Stack2), S2 = [Sum | S1].
+cmd(lte, [Stack1, Stack2 | S1], S2) :- Comp = (Stack1 =< Stack2 -> Res=t;Res=f), call(Comp), S2 = [Res | S1].
 
 % 2. Define the predicate `prog/3`, which describes the effect of executing a
 %    program on the stack.
